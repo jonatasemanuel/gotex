379e2e520037052d2c9794230de6532082786c9b
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2024-01-08 18:40:15 -0300

    feat: note crud handlers

--- gote-server/controllers/note.go
+++ gote-server/controllers/note.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/jonatasemanuel/gote-server/helpers"
 	"github.com/jonatasemanuel/gote-server/services"
 )
@@ -37,4 +38,45 @@ func GetAllNotes(w http.ResponseWriter, r *http.Request) {
 	}
 
 	helpers.WriteJSON(w, http.StatusOK, helpers.Envelop{"notes": all})
+}
+
+// GET/notes/{id}
+func GetNoteById(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	note, err := note.GetNoteById(id)
+
+	if err != nil {
+		helpers.MessageLogs.ErrorLog.Println(err)
+		return
+	}
+	helpers.WriteJSON(w, http.StatusOK, note)
+}
+
+// PUT/notes/{id}
+func UpdateNote(w http.ResponseWriter, r *http.Request) {
+	var noteData services.Note
+
+	id := chi.URLParam(r, "id")
+	err := json.NewDecoder(r.Body).Decode(&noteData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	noteUpdated, err := note.UpdateNote(id, noteData)
+	if err != nil {
+		helpers.MessageLogs.ErrorLog.Println(err)
+	}
+
+	helpers.WriteJSON(w, http.StatusOK, noteUpdated)
+}
+
+// DELETE/notes/{id}
+func DeleteNote(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if err := note.DeleteNote(id); err != nil {
+		helpers.MessageLogs.ErrorLog.Println(err)
+	}
+
+	helpers.WriteJSON(w, http.StatusOK, helpers.Envelop{"message": "Successfull deletion"})
 }
